f34f16ae5d84a4a82927483b02dab02742aa07ff
adeunsbha-techd <adeunsbha@gmail.com>
2026-09-25 17:42:58 +0700

    Hardcode ENV buat Railway

--- start.js
+++ start.js
@@ -1,14 +1,9 @@
+process.env.DISCORD_TOKEN = "MTU1Mjk4NzQxMDQ0OTY5NDgyMQ.Ggpl4X.rFb6scbToIuRqfWqSeEApMHw7zqrpqRs-0Q3S0";
+process.env.CLIENT_ID = "1552987410449694821";
+process.env.GUILD_ID = "1510255795227590657";
+
 require('./bot.js');
 require('./events.js');
 const { client, CONFIG } = require('./bot.js');
-
-if (!CONFIG.token || CONFIG.token.includes('ISI_')) {
-  console.error('❌ Token belum diisi!');
-  process.exit(1);
-}
-if (!CONFIG.guildId || CONFIG.guildId.includes('ISI_')) {
-  console.error('❌ Guild ID belum diisi di config.json!');
-  process.exit(1);
-}
 
 client.login(CONFIG.token);
