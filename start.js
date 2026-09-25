000e71617fdd68a7edd6ec80fe31450390372fb2
adeunsbha-techd <adeunsbha@gmail.com>
2026-09-25 17:44:28 +0700

    Remove token dari code

--- start.js
+++ start.js
@@ -1,9 +1,5 @@
-process.env.DISCORD_TOKEN = "MTU1Mjk4NzQxMDQ0OTY5NDgyMQ.Ggpl4X.rFb6scbToIuRqfWqSeEApMHw7zqrpqRs-0Q3S0";
-process.env.CLIENT_ID = "1552987410449694821";
-process.env.GUILD_ID = "1510255795227590657";
-
+// Token & ID diambil dari Environment Variables (Railway)
 require('./bot.js');
 require('./events.js');
 const { client, CONFIG } = require('./bot.js');
-
 client.login(CONFIG.token);
